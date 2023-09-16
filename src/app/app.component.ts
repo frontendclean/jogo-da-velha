56d7aeef1a7e78c553f93a27574a67d820f6d986
frontend_clean <desenvjhones@gmail.com>
2023-09-15 21:28:35 -0300

    criando o projeto com standalone components

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { SettingsComponent } from './pages/settings/settings.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, SettingsComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+}
