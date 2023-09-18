211d24a10f31549ba337472e0ba4df3b93e9d379
frontend_clean <desenvjhones@gmail.com>
2023-09-18 20:29:11 -0300

    construindo a estrutura do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SettingsComponent } from './pages/settings/settings.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SettingsComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
